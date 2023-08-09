e51a4196260947b35c90707695f438354fd69574
Yuerou Tang <yuerou.tang@berkeley.edu>
2023-08-08 17:59:45 -0700

    extract extension 1

--- src/gradescope_api/assignment.py
+++ src/gradescope_api/assignment.py
@@ -89,6 +89,70 @@ class GradescopeAssignment:
             url, headers=headers, json=payload, timeout=20)
         check_response(response, "creating an extension failed")
 
+    def get_extension(self, email: str):
+        """
+        A new method to apply an extension to a Gradescope assignment, given an email and a number of days.
+        """
+        # First, fetch the extensions page for the assignment, which contains a student roster as well as
+        # the due date (and hard due date) for the assignment.
+        course_id = self._course.course_id
+        assignment_id = self.assignment_id
+        response = self._client.session.get(
+            f"https://www.gradescope.com/courses/{course_id}/assignments/{assignment_id}/extensions", timeout=20
+        )
+        check_response(response, "could not load assignment")
+
+        # Once we fetch the page, parse out the data (students + due dates)
+        soup = BeautifulSoup(response.content, "html.parser")
+        props = soup.find(
+            "li", {"data-react-class": "AddExtension"})["data-react-props"]
+        data = json.loads(props)
+        students = {row["email"]: row["id"]
+                    for row in data.get("students", [])}
+        #user_id = students.get(email)
+        #if not user_id:
+        #    raise GradescopeAPIError("student email not found")
+
+        # A helper method to transform the date
+        def transform_date(datestr: str):
+            dt = pytz.timezone("US/Pacific").localize(parse(datestr))
+            dt = dt + timedelta(num_days)
+            return dt.astimezone(pytz.utc)
+
+        assignment = data["assignment"]
+        new_due_date = transform_date(assignment["due_date"])
+        print(new_due_date)
+
+        '''if assignment["hard_due_date"]:
+            new_hard_due_date = transform_date(assignment["hard_due_date"])
+
+        # Make the post request to create the extension
+        url = self.get_url() + "/extensions"
+        headers = {
+            "Host": "www.gradescope.com",
+            "Origin": "https://www.gradescope.com",
+            "Referer": url,
+            "X-CSRF-Token": self._client._get_token(url, meta="csrf-token"),
+        }
+        payload = {
+            "override": {
+                "user_id": user_id,
+                "settings": {
+                    "due_date": {"type": "absolute", "value": new_due_date.strftime(GRADESCOPE_DATETIME_FORMAT)}
+                },
+            }
+        }
+
+        if assignment["hard_due_date"]:
+            payload["override"]["settings"]["hard_due_date"] = {
+                "type": "absolute",
+                        "value": new_hard_due_date.strftime(GRADESCOPE_DATETIME_FORMAT),
+            }
+
+        response = self._client.session.post(
+            url, headers=headers, json=payload, timeout=20)
+        check_response(response, "creating an extension failed")'''
+
     # deprecated
     def create_extension(self, user_id: str, due_date: datetime, hard_due_date: Optional[datetime] = None):
         """
